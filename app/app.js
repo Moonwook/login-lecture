737caed8fd6686204c257661b6d80b45a4926129
Moonwook <hi94028@gmail.com>
2022-03-08 00:58:19 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+const home = require("./src/routes/home");
+app.use("/", home);
+
+module.exports = app;
